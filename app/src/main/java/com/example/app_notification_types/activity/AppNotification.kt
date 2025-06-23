69e5bd1c1f0ff2fb375044c2e0845a18feb1254f
vigoinfotech <ajay.vigoinfotech@gmail.com>
2025-06-23 19:20:30 +0530

    Notifications types

--- app/src/main/java/com/example/app_notification_types/activity/AppNotification.kt
+++ app/src/main/java/com/example/app_notification_types/activity/AppNotification.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -31,6 +32,7 @@ class AppNotification : AppCompatActivity() {
         createNotificationChannel()
 
         // Set click listener on the button to show the notification
+        //Simple Notification
         binding.btn1.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
@@ -41,6 +43,79 @@ class AppNotification : AppCompatActivity() {
                 .setContentTitle("Test Notification")
                 .setContentText("This is a test notification")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(NOTIFICATION_ID, builder.build())
+            }
+        }
+
+        //Inbox Style Notification
+        binding.btn2.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.app_icon)
+                .setContentTitle("5 new emails")
+                .setStyle(NotificationCompat.InboxStyle()
+                    .addLine("Email 1")
+                    .addLine("Email 2")
+                    .addLine("Email 3"))
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(NOTIFICATION_ID, builder.build())
+            }
+        }
+
+        //Big Picture Notification
+        binding.btn3.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.app_icon)
+                .setContentTitle("Image Notification")
+                .setStyle(NotificationCompat.BigPictureStyle()
+                    .bigPicture(BitmapFactory.decodeResource(resources, R.drawable.app_icon)))
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(NOTIFICATION_ID, builder.build())
+            }
+        }
+
+        //Progress Notification
+        binding.btn4.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+
+            val progress = 50
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Download in progress")
+                .setSmallIcon(R.drawable.app_icon)
+                .setProgress(100, progress, false)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(NOTIFICATION_ID, builder.build())
+            }
+        }
+
+        //Heads-up Notification
+        binding.btn5.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.app_icon)
+                .setContentTitle("Urgent Alert")
+                .setContentText("Respond immediately")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL) // Sound, vibration, etc.
+
 
             with(NotificationManagerCompat.from(this)) {
                 notify(NOTIFICATION_ID, builder.build())
@@ -62,3 +137,4 @@ class AppNotification : AppCompatActivity() {
         }
     }
 }
+
